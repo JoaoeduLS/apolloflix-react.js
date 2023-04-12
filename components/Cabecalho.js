0e83a0adbb1e4459b05a6646184261997d95effe
Hugo Loiola <hugoloiola.1@gmail.com>
2023-04-12 19:37:21 -0300

    criar componentes, instalar bootstrap e axios

---
+++ components/Cabecalho.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const Cabecalho = (props) => {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Início</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/array">Array</Nav.Link>
+              <Nav.Link href="/objeto">Objeto</Nav.Link>
+              <Nav.Link href="/carro">Carro</Nav.Link>
+              <Nav.Link href="/filmes">Filmes</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Cabecalho;
