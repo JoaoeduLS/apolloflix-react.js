3d07697521d5094d934e82d76f98e54d659a60b2
Hugo Loiola <hugoloiola.1@gmail.com>
2023-04-15 21:05:28 -0300

    pt-br

--- services/apiFilmes.js
+++ services/apiFilmes.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const apiFilmes = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization: "Bearer " + process.env.API_KEY_TMDB,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2OGFiODU1NzA5YjMzMDI0NDQ4ZDAwZGM4MTkwNjNjNCIsInN1YiI6IjY0MzVkZWNjNjUxZmNmMDBmMjZkOGJkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4ih8_mYlITUoLw8K3dZHntQzu2d8ug1TXjAzYaOMWGs",
   },
 });
 
