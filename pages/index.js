5b1e7ae63533c9e9c7f870d4112ee8d0da122682
Hugo Loiola <hugoloiola.1@gmail.com>
2023-04-18 19:37:43 -0300

    aula_4 iniciada

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Pagina from "../components/Pagina";
-import { Col, Row } from "react-bootstrap";
 
 const Home = () => {
   return <Pagina titulo="Página Inicial"></Pagina>;
